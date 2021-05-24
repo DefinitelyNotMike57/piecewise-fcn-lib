bdfc3d95d87a9be909859df0f6376de250043912
Not Mike <DefinitelyNotMike57@gmail.com>
2021-05-24 04:07:20 +0000

    Factory generates both sub-functions

--- src/factory.rs
+++ src/factory.rs
@@ -23,20 +23,44 @@ pub trait SubfunctionFactory {
 }
 
 struct Factory;
-/*
+
 impl SubfunctionFactory for Factory {
-    fn polynomial(&self,dur: f64, interval: (f64,f64), coeff: Vec<f64>, reverse: bool) -> Box<dyn SubfunctionOutput> {
-        let new = Polynomial::new( dur, interval, coeff, reverse );
-        new
-    }
-    fn bump(&self, dur:f64, interval: (f64,f64), scale:f64, offset:f64 ) -> Box<dyn SubfunctionOutput> {
-        let new = Bump::new( dur, interval, scale, offset );
-        new
-    }
+  fn polynomial(
+    &self,
+    dur: f64,
+    interval: (f64, f64),
+    coeff: Vec<f64>,
+    reverse: bool,
+  ) -> Box<dyn SubfunctionOutput> {
+    let new = Box::new(polynomial::Polynomial::new(dur, interval, coeff, reverse));
+    new
+  }
+  fn bump(
+    &self,
+    dur: f64,
+    interval: (f64, f64),
+    scale: f64,
+    offset: f64,
+  ) -> Box<dyn SubfunctionOutput> {
+    let new = Box::new(bump::Bump::new(dur, interval, scale, offset));
+    new
+  }
 }
-*/
 
 #[cfg(test)]
-mod factory {
+mod tests {
   use super::*;
+
+  #[test]
+  fn polynomial() {
+    let factory = Factory;
+    let a = factory.polynomial(1.0, (0.0, 1.0), vec![4.0, 5.0], false);
+    assert_eq!(Some(6.5), a.generate(0.5));
+  }
+  #[test]
+  fn bump() {
+    let factory = Factory;
+    let a = factory.bump( 1.0, (-1.0, 1.0), 1.0, 0.0 );
+    assert_eq!(Some(1.0), a.generate(0.5));
+  }
 }
